df3f677576a13a31a505876a3f03f8b82be77265
zzbin123456 <164109059@qq.com>
2017-07-11 12:16:00 +0800

    2017-07-11

---
+++ src/prs/concurrentinaction/c3/TestFinal.java
@@ -0,0 +1,27 @@
+package prs.concurrentinaction.c3;
+
+/**
+ * Created by zhangbin on 2017/7/10.
+ */
+public class TestFinal {
+
+    private final Person person;
+
+    public TestFinal() {
+        this.person = new Person();
+        person.setName("1");
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public static void main(String[] args) {
+        TestFinal t = new TestFinal();
+        Person p = t.getPerson();
+        p.setName("2");
+        p.setName("3");
+        System.out.println(p.getName());
+    }
+
+}
---
+++ src/prs/concurrentinaction/c3/TestThreadLocal.java
@@ -0,0 +1,26 @@
+package prs.concurrentinaction.c3;
+
+import java.sql.*;
+
+/**
+ * Created by zhangbin on 2017/7/10.
+ */
+public class TestThreadLocal {
+
+    private static ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>(){
+        @Override
+        protected Connection initialValue() {
+            try {
+                return java.sql.DriverManager.getConnection("");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
+
+    public static Connection getConnection(){
+        return connectionHolder.get();
+    }
+
+}
